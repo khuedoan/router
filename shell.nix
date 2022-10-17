ad9fbcb9d689a442c08828172dceba11b8b47439
Khue Doan <mail@khuedoan.com>
2022-10-17 14:13:21 +0700

    build: add build config files

---
+++ shell.nix
@@ -0,0 +1,15 @@
+# https://status.nixos.org
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ec96702d7dbc5d3af40ef1c4d28040ec4a0f11e8.tar.gz") {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    ansible
+    ansible-lint
+    bmake
+    git
+    openssh
+    packer
+    pre-commit
+    shellcheck
+  ];
+}
